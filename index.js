064ef243c19772df6a02a15464c78fc86f0fa204
Markus Felten <Markus.Felten@gmx.de>
2015-12-28 06:46:17 +0100

    feat: show how bidi endpoints could work

--- index.js
+++ index.js
@@ -3,25 +3,46 @@
 const Koa = require('koa');
 const app = new Koa();
 
+let endpoint1 = {
+  get name() {
+    "ep1"
+  },
+  feed: undefined
+};
 
-let it = mygen();
+
+let endpoint2 = {
+  get name() {
+    "ep2"
+  },
+  connect(e) {
+    this.counterpart = e;
+  },
+  set feed(feed) {
+    this.counterpart.feed = feed;
+  }
+}
+
+endpoint2.connect(endpoint1);
+
+
+endpoint2.feed = function* () {
+  let seq = 1;
+  let request = yield;
+
+  while (true) {
+    request = yield `${request.url} ${seq}`;
+
+    console.log(`got request: ${request.url}`);
+    seq += 1;
+  }
+};
+
+
+let it = endpoint1.feed();
 
 app.use(ctx => {
-
-  ctx.body = it.next(ctx.request.url).value;
+  ctx.body = it.next(ctx.request).value;
 });
 
 app.listen(3000);
-
-
-function* mygen() {
-  let seq = 1;
-  let request;
-
-  while (true) {
-    request = yield `${request} ${seq}`;
-
-    console.log(`got request: ${request}`);
-    seq += 1;
-  }
-}
